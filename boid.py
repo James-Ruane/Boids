ee2caa06b3b483e086fe58459cd93514e96f9480
James-Ruane <JamesRuane01@gmail.com>
2023-10-22 13:27:17 +0100

    optimise boids

--- boid.py
+++ boid.py
@@ -1,14 +1,15 @@
 import random
+from quadtree import *
 from py5 import Py5Vector
 
 class Boid():
     def __init__(self):
         from main import x_screen_size, y_screen_size
         self.position = Py5Vector(random.randint(0,x_screen_size),random.randint(0,y_screen_size))
-        self.velocity = Py5Vector(random.random(),random.random())
+        self.velocity = Py5Vector(random.uniform(-1,1),random.uniform(-1,1))
         self.velocity.set_mag(random.randint(2,4))
         self.acceleration = Py5Vector(random.uniform(-1,1),random.uniform(-1,1))
-        self.vision = 50
+        self.vision = 100
         self.maxSpeed = 4
         self.maxForce = 0.2
 
@@ -30,62 +31,41 @@ class Boid():
 
     def flock(self, Boids):
         acceleration = Py5Vector(0,0)
-        allign = Py5Vector(Boid.allign(self, Boids))
-        cohesion = Py5Vector(Boid.cohes(self, Boids))
-        separation = Py5Vector(Boid.sep(self, Boids))
-        acceleration = Py5Vector(acceleration) + Py5Vector(allign) 
-        acceleration =  Py5Vector(acceleration) + Py5Vector(cohesion) 
-        acceleration = Py5Vector(acceleration) + Py5Vector(separation)
+        x, y, z = self.flocking_rules(Boids)
+
+        acceleration = Py5Vector(acceleration) + Py5Vector(x) 
+        acceleration =  Py5Vector(acceleration) + Py5Vector(y) 
+        acceleration = Py5Vector(acceleration) + Py5Vector(z) 
+        
         self.acceleration = acceleration
-    
-    def allign(self, flock):
-        average = Py5Vector(0,0)
-        vision = self.vision
-        total = 0
-        for i in flock:
-            distance = Py5Vector.dist(self.position, i.position)
-            if (self != i and distance < vision):
-                average = Py5Vector(average) + i.velocity
-                total += 1
-        if total > 0:
-            average = Py5Vector(average) / total
-            average.set_mag(self.maxSpeed)
-            average = Py5Vector(average) - self.velocity
-            average.set_limit(self.maxForce)
-        return average
-    
-    def cohes(self, flock):
-        average = Py5Vector(0,0)
-        vision = self.vision
-        total = 0
-        for i in flock:
-            distance = Py5Vector.dist(self.position, i.position)
-            if (self != i and distance < vision):
-                average = Py5Vector(average) + i.position
-                total += 1
-        if total > 0:
-            average = Py5Vector(average) / total
-            average = Py5Vector(average) - self.position
-            average.set_mag(self.maxSpeed)
-            average = Py5Vector(average) - self.velocity
-            average.set_limit(self.maxForce)
-        return average
 
-    def sep(self, flock):
-        average = Py5Vector(0,0)
-        vision = self.vision
-        total = 0
-        for i in flock:
-            distance = Py5Vector.dist(self.position, i.position)
-            if (self != i and distance < vision):
-                diff = self.position - Py5Vector(i.position)
-                diff = Py5Vector(diff) / distance 
-                average = Py5Vector(average) + diff
-                total += 1
-        if total > 0:
-            average = Py5Vector(average) / total
-            average.set_mag(self.maxSpeed)
-            average = Py5Vector(average) - self.velocity
-            average.set_limit(self.maxForce)
-        return average
-
+    def flocking_rules(self, flock):
+            average_allign = Py5Vector(0,0) 
+            average_cohesion = Py5Vector(0,0) 
+            average_sep = Py5Vector(0,0)
+            vision = self.vision
+            total = 0
+            for i in flock:
+                # i = i.userData
+                distance = Py5Vector.dist(self.position, i.position)
+                if (self != i and distance < vision):
+                    average_allign = Py5Vector(average_allign) + i.velocity
+                    average_cohesion = Py5Vector(average_cohesion) + i.position
+                    diff = self.position - Py5Vector(i.position)
+                    diff = Py5Vector(diff) / distance 
+                    average_sep = Py5Vector(average_sep) + diff
+                    total += 1
+            if total > 0:
+                average_allign = self.calc_avg(average_allign, total)
+                average_cohesion = self.calc_avg(average_cohesion, total, True)
+                average_sep = self.calc_avg(average_sep, total)
+            return average_allign, average_cohesion, average_sep
+    
+    def calc_avg(self, v, total, coh = False):
+        v = Py5Vector(v) / total
+        if coh:
+            v = Py5Vector(v) - self.position
+        v.set_mag(self.maxSpeed)
+        v = Py5Vector(v) - self.velocity
+        v.set_limit(self.maxForce)
+        return v
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
 import random
 from boid import Boid
+from quadtree import *
 
 global screen_size
 
 # setup
-x_screen_size = 500
-y_screen_size = 500
+x_screen_size = 1000
+y_screen_size = 1000
 pygame.init()
 screen = pygame.display.set_mode((x_screen_size, y_screen_size))
 clock = pygame.time.Clock()
@@ -17,7 +18,7 @@ pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 # creates initial flock
 flock = []
-for i in range(30):
+for i in range(75):
     flock.append(Boid())
 
 while running:
@@ -27,7 +28,7 @@ while running:
             running = False
 
     # fill screen to wipe last frame
-    screen.fill("black")
+    screen.fill("lightblue")
 
     # draws the border
     y_up_lim = y_screen_size - 50
@@ -39,9 +40,26 @@ while running:
     pygame.draw.line(screen, "white", [x_low_lim, y_up_lim], [x_up_lim, y_up_lim], 5)
     pygame.draw.line(screen, "white", [x_up_lim, y_low_lim], [x_up_lim, y_up_lim], 5)
 
+
+
+    # boundary = rectangle(screen.get_width() / 2,screen.get_height() / 2, screen.get_width(), screen.get_height())
+    # qt = quadTree(boundary, 4)
+
+    # for i in flock:
+    #     #draws boid
+    #     pygame.draw.circle(screen, "black", i.position, 5)
+    #     Boid.border(i)
+    #     i_point = point(i.position[0], i.position[1], i)
+    #     qt.insert(i_point)
+    #     range = rectangle(i.position[0], i.position[1], i.vision, i.vision)
+    #     points = qt.query(range, [])
+    #     Boid.flock(i, points)
+    #     Boid.update(i)
+
+
     for i in flock:
         #draws boid
-        pygame.draw.circle(screen, "red", i.position, 5)
+        pygame.draw.circle(screen, "black", i.position, 5)
         Boid.border(i)
         Boid.flock(i, flock)
         Boid.update(i)
